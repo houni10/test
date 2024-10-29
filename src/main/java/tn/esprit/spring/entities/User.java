bc2f6844b27c38557c7a0b595934e57751e148c7
henir <heni.rebhi@esprit.t>
2024-10-29 01:20:14 +0100

    modification 2

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -28,7 +28,7 @@ public class User implements Serializable {
 	private String firstName; 
 	
 	private String lastName;
-	
+	private  String king;
 	@Temporal(TemporalType.DATE)
 	private Date dateNaissance;
 	
